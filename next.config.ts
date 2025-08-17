9c8adc3480bf83c7dd4c1260cdcd11188852e21b
predragBgd <pecabegd@gmail.com>
2025-08-17 12:10:40 +0200

    Small Changes on deploy.yml

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/public-web-app/", // ime tvog repo-a
+  basePath: "/public-web-app", // ime tvog repo-a
   images: {
     unoptimized: true, // jer GitHub Pages ne podržava Image Optimization
   },
