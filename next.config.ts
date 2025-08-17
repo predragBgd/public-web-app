4d77b33e76122d1ad4aab1a8107a0ffa5520a381
predragBgd <pecabegd@gmail.com>
2025-08-17 12:16:58 +0200

    setup for GitHub Pages deploy

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/public-web-app", // ime tvog repo-a
-  images: {
-    unoptimized: true, // jer GitHub Pages ne podržava Image Optimization
+  basePath: "/public-web-app", // ⚠ ne završavaj sa /
+  images: { unoptimized: true },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@root": path.resolve(__dirname), // mapa tsconfig alias-a na root folder
+    };
+    return config;
   },
 };
 
